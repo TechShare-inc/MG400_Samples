5e07fdaf4fd7a7cdcbbf71236178fdeea23f4576
hoshianaaa <hoshina12345@gmail.com>
2021-05-11 21:38:38 +0900

    ポンプのオンオフ対応

--- TCP_motion_client.py
+++ TCP_motion_client.py
@@ -6,12 +6,22 @@ import os
 import struct
 import ctypes
 
-class PacketUDP(ctypes.Structure):
+class PacketMoveXYZ(ctypes.Structure):
     _fields_ = [
+        ("id", ctypes.c_float),
         ("x", ctypes.c_float),
         ("y", ctypes.c_float),
         ("z", ctypes.c_float),
     ]
+
+
+class PacketPomp(ctypes.Structure):
+    _fields_ = [
+        ("id", ctypes.c_float),
+        ("pwr", ctypes.c_float),
+        ("dir", ctypes.c_float),
+    ]
+
 
 target_ip = "192.168.2.6"
 target_port = 6001
@@ -27,15 +37,28 @@ tcp_client.connect((target_ip,target_port))
 def move_xyz(x,y,z):
 
     # 3.サーバにデータを送信
-    data = PacketUDP(x,y,z)
+    data = PacketMoveXYZ(1,x,y,z)
     tcp_client.send(data)
 
     # 4.サーバからのレスポンスを受信
     response = tcp_client.recv(300)
     print("[*]Received a response : {}".format(response))
 
+def pomp(p,d):
+
+    # 3.サーバにデータを送信
+    data = PacketPomp(2,p,d)
+    tcp_client.send(data)
+
+    # 4.サーバからのレスポンスを受信
+    response = tcp_client.recv(300)
+    print("[*]Received a response : {}".format(response))
+
+
 move_xyz(300,0,50)
 
 while True:
     move_xyz(300,-50,50)
+    pomp(1,1)
     move_xyz(300,50,50)
+    pomp(0,0)
