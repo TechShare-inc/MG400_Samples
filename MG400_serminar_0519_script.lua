a667e66ed68641430265d3462e6462640a6c84f6
hoshianaaa <40942409+hoshianaaa@users.noreply.github.com>
2021-05-13 17:34:21 +0900

    Update MG400_serminar_0519_script.lua

--- MG400_serminar_0519_script.lua
+++ MG400_serminar_0519_script.lua
@@ -14,6 +14,20 @@ function Go_xyzr(x,y,z,r)
 	
 end
 
+function MoveL_xyzr(x,y,z,r)
+
+	P = {}
+	P["tool"] = 0
+	P["armOrientation"]=right
+	P["load"]=0.000000 
+	P["coordinate"]={x, y, z, r, 0.000000, 0.000000 }
+	P["user"]=0.000000
+
+	-- SYNC=1が重要,ロボットが移動終了するのを待機
+	MovL(P)
+  
+end
+
 function PompON()
   
   DO(1,ON)
@@ -30,27 +44,29 @@ end
         
 local x = 280
 local y = 125
-local z = -78
+local z = -120
 
 Go_xyzr(x,0,0,0)
         
 while true do
           
- Go_xyzr(x,0,0,0)
  Go_xyzr(x,y,0,0)
- Go_xyzr(x,y,z,0)
- Go_xyzr(x,y,0,0)
-          
+ MoveL_xyzr(x,y,z,0)
+
  PompON()
+ Wait(500)
+          
+ MoveL_xyzr(x,y,0,0)
           
  y = -y
           
  Go_xyzr(x,y,0,0)
- Go_xyzr(x,y,z,0)
- Go_xyzr(x,y,0,0)
+ MoveL_xyzr(x,y,z,0)
+ PompOFF()
+ Wait(500)
+  
+ MoveL_xyzr(x,y,0,0)
  
  Go_xyzr(x,0,0,0)
-          
- PompOFF()
           
 end
